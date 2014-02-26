151f7d81de918b2b681dde632e5b6269c21c2877
equigley <equigley@tcd.ie>
2014-02-26 18:17:42 +0000

    got rid of serial algorithm for testing

--- matrix.c
+++ matrix.c
@@ -2,23 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-void matmul(double ** A, double ** B, double ** C, int row, int col, int common)
-{
-
-
-	for ( int i = 0; i < row; i++) 
-	{
- 		for (int j = 0; j < col; j++)
-		{
-			int sum = 0;
-			for (int k = 0; k < common; k++)
-			{
-				sum += A[i][k] * B[k][j];
-			}
-			C[i][j] = sum;
-		}
-	}
- }
+ 
 
 void matmul_par(double ** A, double ** B, double ** C, int row, int col, int common)
 {
@@ -59,17 +43,11 @@ void matmul_par(double ** A, double ** B, double ** C, int row, int col, int com
 	for( i = 0; i< a_row; i++){
 		c[i] = malloc(sizeof(double) * b_col);
 	}
-
-	clock_t start = clock();
-	matmul(a,b,c, a_row,b_col, a_col_b_row);
-
-	clock_t finish = clock() - start;
-	printf("%lu\n", finish );
-
-	clock_t startt = clock();
+ 
 	matmul_par(a,b,c, a_row,b_col, a_col_b_row);
-	clock_t finnish = (clock() - startt)/64;
-	printf("%lu\n", finnish );
+
+	 
+ 
 	return 0;
  }
 
